051395fd7f89184fc3b102ece3fe5bc20d4373be
Cipto Tri Utomo <ciptredevie@gmail.com>
2022-03-24 10:03:50 +0700

    setting husky for commit tester

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>asdasd</p>
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello Vite + React!</p>
         <p>
